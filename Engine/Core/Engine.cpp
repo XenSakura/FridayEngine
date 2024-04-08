a75004d19a3eb4bf7bc008aaafb26d407fe5b935
XenSakura <XenSakura@users.noreply.github.com>
2024-04-08 03:19:31 -0700

    Debug callbacks implemented
    Debug callback for Vulkan implemented

--- Engine/Core/Engine.cpp
+++ Engine/Core/Engine.cpp
@@ -1,21 +1,200 @@
+/*****************************************************************//**
+ * @file   Engine.cpp
+ * @brief  Main Game Engine file
+ * 
+ * @author Sakura
+ * @date   April 2024
+ *********************************************************************/
 #include "Engine.h"
+#include <iostream>
+#include <stdexcept>
+/**
+ * @brief Engine Constructor.
+ * 
+ */
 Engine::Engine()
-	:prevTime(std::chrono::steady_clock::now())
+    : prevTime(std::chrono::steady_clock::now()),
+    renderInstance(std::make_unique<RenderSystem>())
 {
-
+    GLFWSetup();
 }
 
-void Engine::Update()
+/**
+ * @brief Engine's main run loop.
+ * 
+ */
+void Engine::Run()
 {
-	auto currentTime = std::chrono::steady_clock::now();
-	std::chrono::duration<float> deltaTime = currentTime - prevTime;
-	float dt = deltaTime.count();
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        auto currentTime = std::chrono::steady_clock::now();
+        std::chrono::duration<float> deltaTime = currentTime - prevTime;
+        float dt = deltaTime.count();
 
-	prevTime = currentTime;
+        renderInstance->Update(dt);
+
+        prevTime = currentTime;
+    }
 }
-
+/**
+ * @brief Engine Destructor.
+ * 
+ */
 Engine::~Engine()
 {
-	
+    vkDestroyInstance(instance, nullptr);
+    GLFWCleanup();
 }
+/**
+ * @brief GLFW setup commands.
+ * 
+ */
+void Engine::GLFWSetup()
+{
+    glfwInit();
+    window = glfwCreateWindow(WIDTH, HEIGHT, "FridayEngine", nullptr, nullptr);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+}
+/**
+ * @brief GLFW cleanup.
+ * 
+ */
+void Engine::GLFWCleanup()
+{
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+/**
+ * @brief Setting up Vulkan.
+ * 
+ */
+void Engine::VulkanSetup()
+{
+    CreateInstance();
+}
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    }
+    else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL Engine::DebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+    return VK_FALSE;
+}
+
+void Engine::PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+    createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo.pfnUserCallback = DebugCallback;
+}
+
+void Engine::SetupDebugMessenger() {
+    if (!enableValidationLayers) return;
+
+    VkDebugUtilsMessengerCreateInfoEXT createInfo;
+    PopulateDebugMessengerCreateInfo(createInfo);
+
+    if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+        throw std::runtime_error("failed to set up debug messenger!");
+    }
+}
+
+void Engine::CreateInstance() {
+    if (enableValidationLayers && !CheckValidationLayerSupport()) {
+        throw std::runtime_error("validation layers requested, but not available!");
+    }
+
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    auto extensions = GetRequiredExtensions();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
+
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+    if (enableValidationLayers) {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        PopulateDebugMessengerCreateInfo(debugCreateInfo);
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+    }
+    else {
+        createInfo.enabledLayerCount = 0;
+
+        createInfo.pNext = nullptr;
+    }
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
+    }
+}
+
+std::vector<const char*> Engine::GetRequiredExtensions() {
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+    if (enableValidationLayers) {
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    return extensions;
+}
+
+bool Engine::CheckValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
 
--- Engine/Core/Engine.h
+++ Engine/Core/Engine.h
@@ -1,11 +1,78 @@
+/*****************************************************************//**
+ * \file   Engine.h
+ * \brief  Main game engine header
+ * 
+ * \author Sakura
+ * \date   April 2024
+ *********************************************************************/
 #pragma once
 #include <chrono>
+#include <memory>
+#include <vector>
+#include "RenderSystem.h"
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+#include "vulkan/vulkan.h"
+
 class Engine
 {
 public:
-	Engine();
-	void Update();
-	~Engine();
+
+    Engine();
+
+
+    void Run();
+
+
+    ~Engine();
+
 private:
-	std::chrono::steady_clock::time_point prevTime;
+
+    std::chrono::steady_clock::time_point prevTime;
+
+
+    std::unique_ptr<RenderSystem> renderInstance;
+
+
+    GLFWwindow* window;
+
+
+    const uint32_t HEIGHT = 600;
+
+
+    const uint32_t WIDTH = 800;
+
+
+    void GLFWSetup();
+
+
+    void GLFWCleanup();
+
+
+    VkInstance instance;
+
+    void VulkanSetup();
+
+
+    void CreateInstance();
+
+    const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
+    bool CheckValidationLayerSupport();
+
+    std::vector<const char*> GetRequiredExtensions();
+
+    VkDebugUtilsMessengerEXT debugMessenger;
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
+
+    void SetupDebugMessenger();
+
+    void PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
 };
