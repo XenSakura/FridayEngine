1d8459ba27f79ce3cd6ad956b607096311650545
mivimproved <143461207+miVimproved@users.noreply.github.com>
2024-05-07 13:33:22 -0700

    Removed build folder.

---
+++ build/CMakeFiles/FridayEngine.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vimmeous/documents/development/sakuraengine/Engine/Core/Engine.cpp" "CMakeFiles/FridayEngine.dir/Engine/Core/Engine.cpp.o" "gcc" "CMakeFiles/FridayEngine.dir/Engine/Core/Engine.cpp.o.d"
+  "/home/vimmeous/documents/development/sakuraengine/Engine/Core/FridayEngine.cpp" "CMakeFiles/FridayEngine.dir/Engine/Core/FridayEngine.cpp.o" "gcc" "CMakeFiles/FridayEngine.dir/Engine/Core/FridayEngine.cpp.o.d"
+  "/home/vimmeous/documents/development/sakuraengine/Engine/Graphics/RenderSystem.cpp" "CMakeFiles/FridayEngine.dir/Engine/Graphics/RenderSystem.cpp.o" "gcc" "CMakeFiles/FridayEngine.dir/Engine/Graphics/RenderSystem.cpp.o.d"
+  "/home/vimmeous/documents/development/sakuraengine/Engine/Graphics/VulkanRenderAPI.cpp" "CMakeFiles/FridayEngine.dir/Engine/Graphics/VulkanRenderAPI.cpp.o" "gcc" "CMakeFiles/FridayEngine.dir/Engine/Graphics/VulkanRenderAPI.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/FridayEngine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FridayEngine.dir/Engine/Core/Engine.cpp.o"
+  "CMakeFiles/FridayEngine.dir/Engine/Core/Engine.cpp.o.d"
+  "CMakeFiles/FridayEngine.dir/Engine/Core/FridayEngine.cpp.o"
+  "CMakeFiles/FridayEngine.dir/Engine/Core/FridayEngine.cpp.o.d"
+  "CMakeFiles/FridayEngine.dir/Engine/Graphics/RenderSystem.cpp.o"
+  "CMakeFiles/FridayEngine.dir/Engine/Graphics/RenderSystem.cpp.o.d"
+  "CMakeFiles/FridayEngine.dir/Engine/Graphics/VulkanRenderAPI.cpp.o"
+  "CMakeFiles/FridayEngine.dir/Engine/Graphics/VulkanRenderAPI.cpp.o.d"
+  "FridayEngine"
+  "FridayEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FridayEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
