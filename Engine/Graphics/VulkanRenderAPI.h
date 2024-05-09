fda782331d1ecd3e952d6df57ac11abfbdaa63f4
Zeness <71611150+XenSakura@users.noreply.github.com>
2024-05-09 14:00:10 -0700

    Vertex and Index buffers implemented for Vulkan Renderer (#5)
    * glm
    
    * Moved ImGui submodule to Libraries folder
    
    * CMake fixed? Potentially?
    
    * Added build folder to .gitignore
    
    * removing the build folder
    
    * Vertex Buffer implementation
    
    Vertex buffer has now been implemented within the Vulkan Renderer
    
    * Buffer Operations and Abstractions
    
    * Index buffer abstractions
    
    * Merge conflict fix
    
    ---------
    
    Co-authored-by: XenSakura <XenSakura@users.noreply.github.com>

--- Engine/Graphics/VulkanRenderAPI.h
+++ Engine/Graphics/VulkanRenderAPI.h
@@ -57,6 +57,11 @@ struct RenderData
     VkBuffer vertexBuffer;
 
     VkDeviceMemory vertexBufferMemory;
+
+    VkBuffer indexBuffer;
+
+    VkDeviceMemory indexBufferMemory;
+
 };
 
 void VulkanSetup(RenderData& data);
