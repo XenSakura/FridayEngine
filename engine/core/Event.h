998db19b9614a112f18c7dd6389aad9185eee7cb
XenSakura <XenSakura@users.noreply.github.com>
2024-05-20 14:46:01 -0700

    event

--- engine/core/Event.h
+++ engine/core/Event.h
@@ -1,4 +1,22 @@
-class Event
+#include <queue>
+
+template <typename... PayloadType>
+class Publisher
+{
+	Publisher()
+	{
+
+	}
+	
+	std::queue<>
+};
+
+class EventHandler
+{
+
+};
+
+class Subscriber
 {
 
 };
