b2a6d342ee9520d31074f38484d6b7267f3e062f
XenSakura <XenSakura@users.noreply.github.com>
2024-06-13 14:33:52 -0700

    Vulkan boilerplate setup

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -6,21 +6,15 @@
  * @date   April 2024
  *********************************************************************/
 #include "Engine.h"
-#include "..\UnitTests.h"
+//#include "..\UnitTests.h"
 #include <iostream>
 /**
  * @brief Engine Constructor.
  * 
  */
 Engine::Engine()
-    : prevTime(std::chrono::steady_clock::now()),
-    renderInstance(nullptr)
+    : prevTime(std::chrono::steady_clock::now())
 {
-    renderInstance = std::make_unique<RenderSystem>();
-    window = &renderInstance.get()->GetWindow();
-#ifdef _DEBUG
-    RunUnitTests();
-#endif
 }
 
 /**
@@ -29,13 +23,11 @@ Engine::Engine()
  */
 void Engine::Run()
 {
-    while (!glfwWindowShouldClose(window))
+    while (true)
     {
-        glfwPollEvents();
         auto currentTime = std::chrono::steady_clock::now();
         std::chrono::duration<float> deltaTime = currentTime - prevTime;
         float dt = deltaTime.count();
-        renderInstance->render();
         prevTime = currentTime;
     }
 }
